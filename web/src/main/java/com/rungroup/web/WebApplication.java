2adada23f983cdcd711b536ef5cc8249193e83ad
Mahesh Choubey <yash.mahesh.choubey@accenture.com>
2024-09-28 12:02:49 +0530

    Initial commit

---
+++ web/src/main/java/com/rungroup/web/WebApplication.java
@@ -0,0 +1,15 @@
+package com.rungroup.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebApplication.class, args);
+
+		System.out.println("Hello World");
+	}
+
+}
